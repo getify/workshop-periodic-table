c108aefdbfaef4cf5843d4fe2d680c8f78f8a1f1
Kyle Simpson <getify@gmail.com>
2023-02-24 04:33:46 -0600

    implementing option 1 algorithms

--- js/speller.js
+++ js/speller.js
@@ -15,16 +15,42 @@ async function loadPeriodicTable() {
 }
 
 function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+	if (inputWord.length > 0) {
+		// check every element for a symbol matching the next
+		// 1-2 characters of the input word
+		for (let element of elements) {
+			let symbol = element.symbol.toLowerCase();
+			if (symbol.length <= inputWord.length) {
+				if (inputWord.slice(0,symbol.length) == symbol) {
+					// more characters in the input word to try
+					// to match to symbols?
+					if (inputWord.length > symbol.length) {
+						// recurse to check the remainder of the
+						// input word
+						let res = check(inputWord.slice(symbol.length));
+
+						// was the check successful?
+						if (res.length > 0) {
+							return [ symbol, ...res ];
+						}
+					}
+					else {
+						return [ symbol ];
+					}
+				}
+			}
+		}
+	}
 
 	return [];
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
-
-	return {};
+	// search all elements to find the matching
+	// symbol (lowercase)
+	for (let element of elements) {
+		if (element.symbol.toLowerCase() == elementSymbol) {
+			return element;
+		}
+	}
 }
