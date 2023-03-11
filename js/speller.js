fbe5095e5af58f5bcc10a96372e620d7df5535de
Kyle Simpson <getify@gmail.com>
2023-03-11 18:23:26 -0500

    implementing option 3 algorithms

--- js/speller.js
+++ js/speller.js
@@ -18,55 +18,45 @@ async function loadPeriodicTable() {
 	}
 }
 
-function findCandidates(inputWord) {
-	var oneLetterSymbols = new Set();
-	var twoLetterSymbols = new Set();
-
-	for (let i = 0; i < inputWord.length; i++) {
-		// collect one letter symbol options
-		if (inputWord[i] in symbols) {
-			oneLetterSymbols.add(inputWord[i]);
-		}
-
-		// collect two letter symbol options
-		if (i <= (inputWord.length - 2)) {
-			let two = inputWord.slice(i,i+2);
-			if (two in symbols) {
-				twoLetterSymbols.add(two);
-			}
-		}
-	}
-
-	return [ ...twoLetterSymbols, ...oneLetterSymbols ];
-}
-
-function spellWord(candidates,charsLeft) {
+function check(charsLeft) {
 	if (charsLeft.length == 0) {
 		return [];
 	}
 	else {
-		for (let candidate of candidates) {
-			let chunk = charsLeft.slice(0,candidate.length);
-			if (candidate == chunk) {
-				if (charsLeft.length > chunk.length) {
-					let rest = charsLeft.slice(chunk.length);
-					let res = spellWord(candidates,rest);
-					if (res.length > 0) {
-						return [ candidate, ...res ];
+		// check for two letter symbols first
+		if (charsLeft.length >= 2) {
+			let two = charsLeft.slice(0,2);
+			let rest = charsLeft.slice(2);
+			if (two in symbols) {
+				if (rest != "") {
+					let result = [ two, ...check(rest) ];
+					if (result.join("") == charsLeft) {
+						return result;
 					}
 				}
 				else {
-					return [ candidate ];
+					return [ two ];
+				}
+			}
+		}
+		// now check for one letter symbols
+		if (charsLeft.length >= 1) {
+			let one = charsLeft[0];
+			let rest = charsLeft.slice(1);
+			if (one in symbols) {
+				if (rest != "") {
+					let result = [ one, ...check(rest) ];
+					if (result.join("") == charsLeft) {
+						return result;
+					}
+				}
+				else {
+					return [ one ];
 				}
 			}
 		}
 	}
 	return [];
-}
-
-function check(inputWord) {
-	var candidates = findCandidates(inputWord);
-	return spellWord(candidates,inputWord);
 }
 
 function lookup(elementSymbol) {
